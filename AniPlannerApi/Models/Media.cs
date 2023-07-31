9f68494bfe5a4b17065ade25d3fca72f094bca02
Ronaldo <ronaldo@octoco.ltd>
2023-07-31 21:07:39 +0200

    added Add tags to media

--- AniPlannerApi/Models/Media.cs
+++ AniPlannerApi/Models/Media.cs
@@ -8,9 +8,27 @@ public class Media
     [Key]
     public Guid MediaId { get; set; }
     public MediaType Type { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     public MediaStatus Status { get; set; }
-    public string PictureUrl { get; set; }
-    public ICollection<MediaTag> MediaTags { get; set; }
+    public string PictureUrl { get; set; } = string.Empty;
+    public ICollection<MediaTag> MediaTags { get; set; } = new List<MediaTag>();
     public int Episodes { get; set; }
+
+    /// <summary>
+    /// Adds the tags given to the Media object as media tags
+    /// </summary>
+    /// <param name="tags">List of tags.</param>
+    public void AddTags(List<Tag> tags)
+    {
+        foreach (var tag in tags)
+        {
+            MediaTags.Add(new MediaTag
+            {
+                Tag = tag,
+                TagId = tag.TagId,
+                Media = this,
+                MediaId = MediaId
+            });
+        }
+    }
 }
--- AniPlannerApi/Models/MediaTag.cs
+++ AniPlannerApi/Models/MediaTag.cs
@@ -3,7 +3,8 @@ namespace AniPlannerApi.Models;
 public class MediaTag
 {
     public Guid MediaId { get; set; }
-    public Media Media { get; set; }
+    public Media? Media { get; set; }
     public Guid TagId { get; set; }
-    public Tag Tag { get; set; }
+    public Tag? Tag { get; set; }
+    
 }
